86f8075358279199f4c164a8698ce30c05719df8
sincostandra <utandraemail@gmail.com>
2024-11-18 17:49:35 +0700

    emm wait

--- SIJARTA/settings.py
+++ SIJARTA/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hijau',
     'merah',
     'kuning',
     'landing_page',
