3fc03a0d07cc211ee7f0927cd52c5e81038c5e91
akmalnabil22 <akmaln761@gmail.com>
2024-12-01 14:02:21 +0700

    fix update nama bank

--- kuning/views.py
+++ kuning/views.py
@@ -205,6 +205,7 @@ def update_pekerja(request):
         nomorrekening = request.POST.get('rekening')
         npwp = request.POST.get('npwp')
         foto = request.POST.get('foto')
+        print(namabank)
 
         # Update data pada database
         query_str = f"""
@@ -259,6 +260,7 @@ def update_pekerja(request):
         penggunaUpdate['kategori']=[]
         for row in kategori:
             penggunaUpdate['kategori'].append(row['namakategori'])
+        print(penggunaUpdate)
         
         request.session['penggunalogin'] = penggunaUpdate
 
