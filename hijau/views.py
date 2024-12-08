4adae28fcbfc7f689677cd5423f4e6c05586fcb1
nobeljb <nobeljulianbintang@gmail.com>
2024-12-08 17:52:47 +0700

    fix diskon pemesanan jasa

--- hijau/views.py
+++ hijau/views.py
@@ -177,7 +177,7 @@ def create_pemesanan(request, subcategory_id, session, price):
         'harga': price,
         'penggunalogin': penggunalogin,
     }
-    if request.method == 'POST':
+    if request.method == 'POST'and request.headers.get('x-requested-with') == 'XMLHttpRequest':
         form = PemesananForm(request.POST)
         if form.is_valid():
             # Ambil data dari form
@@ -188,8 +188,11 @@ def create_pemesanan(request, subcategory_id, session, price):
             # Cek apakah kode diskon valid
             if diskon_code:
                 query_diskon = f"""
-                SELECT * FROM diskon
-                WHERE kode_diskon = '{diskon_code}'"""
+                SELECT * FROM tr_pembelian_voucher
+                join diskon on kode_diskon = idvoucher
+                WHERE idpelanggan = '{penggunalogin['id_user']}' AND idvoucher = '{diskon_code}' AND tglakhir >= current_date AND tglawal <= current_date
+                ORDER BY tglawal ASC, tglakhir ASC
+                """
                 diskon_data = query(query_diskon)
                 
                 if diskon_data:
@@ -198,19 +201,21 @@ def create_pemesanan(request, subcategory_id, session, price):
                     potongan = float(diskon['potongan'])
                     berhasil_diskon = True
                     
-                    # Cek apakah harga memenuhi syarat minimum transaksi
-                    if float(price) >= min_transaksi:
-                        harga_akhir -= potongan  # Kurangi harga dengan potongan
+                    if float(price) < min_transaksi:
+                        # Diskon tidak valid (harga kurang dari min_transaksi)
+                        return JsonResponse({'success': False, 'message': 'Harga tidak memenuhi syarat minimum transaksi.'})
                     else:
-                        return JsonResponse({
-                            'status': 'error',
-                            'message': f"Transaksi minimum untuk kode diskon ini adalah Rp {min_transaksi:,}.",
-                        })
+                        harga_akhir -= potongan  # Kurangi harga dengan potongan
+                        query_str = f"""
+                        UPDATE tr_pembelian_voucher 
+                        SET telahdigunakan = telahdigunakan + 1
+                        WHERE id_tr_pembelian_voucher = '{diskon['id_tr_pembelian_voucher']}'
+                        """
+                        query(query_str)
+
                 else:
-                    return JsonResponse({
-                        'status': 'error',
-                        'message': "Kode diskon tidak valid.",
-                    })
+                    # Diskon tidak valid (kode diskon tidak ditemukan)
+                    return JsonResponse({'success': False, 'message': 'Kode diskon tidak valid.'})
 
             id_pemesanan = uuid.uuid4()
             # Simpan pemesanan ke database
@@ -236,7 +241,7 @@ def create_pemesanan(request, subcategory_id, session, price):
                 """
             query(query_str)
 
-            return redirect('hijau:view_pemesanan')
+            return JsonResponse({'success': True, 'message': 'Pemesanan berhasil dibuat.'})
 
     else:  # GET request, tampilkan form kosong
         context['form'] = PemesananForm()
