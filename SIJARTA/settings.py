b1cf032f153b5397c5601f95fc4afb1180927f2d
sincostandra <utandraemail@gmail.com>
2024-12-09 19:16:59 +0700

    fix css (hopefully)

--- SIJARTA/settings.py
+++ SIJARTA/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
